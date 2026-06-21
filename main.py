6f364de50de470c8edc08ea928aac08971e3b5ea
c3p31003 <c3p31003@bunkyo.ac.jp>
2026-06-22 08:39:15 +0900

    change management password method

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import requests
 from pathlib import Path
 from send_email import send_email
 from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+import os
 
-# Gmail_api_key
-api_key_path = Path(__file__).with_name("news_api_key.txt")
-api_key_path = api_key_path.read_text(encoding="utf-8").strip()
+# Load environment variables from .env file
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
-# Google_gemini_api_key
-GOOGLE_API_KEY = Path(__file__).with_name("gemini_api_key.txt")
-GOOGLE_API_KEY = GOOGLE_API_KEY.read_text(encoding="utf-8").strip()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+
+
 
 topic = 'amazon'
 
 url = (
     f"https://newsapi.org/v2/everything?"
     f"q={topic}&"
-    f"from=2026-06-20&sortBy=publishedAt&"
+    f"from=2026-06-21&sortBy=publishedAt&"
     f"pageSize=3&"
-    f"apiKey={api_key_path}&"
+    f"apiKey={NEWS_API_KEY}&"
     f"language=en"
 )
 # Make request
@@ -32,7 +35,7 @@ content = request.json()
 articles = content["articles"][:5]
 
 
-# AI summarizing the news
+#AI summarizing the news
 model = init_chat_model(
     model="gemini-2.5-flash",
     model_provider="google-genai",
--- send_email.py
+++ send_email.py
@@ -2,17 +2,21 @@ import smtplib
 import ssl
 from email.message import EmailMessage
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
-mail_password = Path(__file__).with_name("mail_info.txt")
-mail_password = mail_password.read_text(encoding="utf-8").strip()
+# Load environment variables from .env file
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "ishiharu5232@gmail.com"
-    password = mail_password
+    password = MAIL_PASSWORD
 
     receiver = "ishiharu5232@gmail.com"
     context = ssl.create_default_context()
@@ -21,7 +25,7 @@ def send_email(message):
         message = message.decode('utf-8')
     
     msg = EmailMessage()
-    msg.set_content(message)          # 本文をセット（日本語も自動で正しくエンコードされます）
+    msg.set_content(message)          
     msg['Subject'] = 'News Summary'   # 件名
     msg['From'] = username            # 送信元
     msg['To'] = receiver              # 送信先
